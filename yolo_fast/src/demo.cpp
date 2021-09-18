23154016ff88de9b1007625a476c7914fbe127b8
MGLF <ganlifu147@163.com>
2021-09-18 17:02:58 +0800

    add yolo to ros

--- yolo_fast/src/demo.cpp
+++ yolo_fast/src/demo.cpp
@@ -1,11 +1,28 @@
 #include<iostream>
 #include "yolo-fastestv2.h"
 #include<ctime>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "yolo_fast/position.h"
 using namespace cv;
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {   
+    // ros初始化
+    ros::init(argc, argv, "yolo_fast");          //创建一个node
+    ros::NodeHandle n;                        //创建一个句柄
+    ros::NodeHandle private_nh("~");
+    ros::Publisher box_pub = n.advertise<yolo_fast::position>("ob_dectetion", 1000);
+    std::string model_param_file,model_bin_file;
+    //设置默认参数
+    //model_param_file = "/opt.param";
+    //model_bin_file = "/opt.bin";
+    
+    private_nh.param<std::string>("model_param_file", model_param_file,"/opt.param");
+    private_nh.param<std::string>("model_bin_file", model_bin_file,"/opt.bin");
+ 
+
     static const char* class_names[] = {
         "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
         "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
@@ -20,29 +37,26 @@ int main()
     
     yoloFastestv2 api;
     clock_t startTime,endTime;
-
-    api.loadModel("/home/xd1/My_data/ros_moveit/src/yolo_fast/model/yolo-fastestv2-opt.param",
-                  "/home/xd1/My_data/ros_moveit/src/yolo_fast/model/yolo-fastestv2-opt.bin");
+    yolo_fast::position obj_position; 
+    api.loadModel(model_param_file.c_str(),model_bin_file.c_str());
 
        
     VideoCapture capture(0);
     cv::Mat cvImg;
     std::vector<TargetBox> boxes;
-    while(true)
+    while(ros::ok())
     { 
         capture>>cvImg;
-        imshow("读取视频", cvImg);
         waitKey(30);//延时30毫秒
         startTime = clock();//计时开始
         api.detection(cvImg, boxes);
         endTime = clock();//计时结束
-        cout << "The run time is: " <<(double)(endTime - startTime) / CLOCKS_PER_SEC *1000 << "ms" << endl;
-        //printf("test");
-        	
+        //cout <<cvImg.size(); //640×480
+        //cout << "The run time is: " <<(double)(endTime - startTime) / CLOCKS_PER_SEC *1000 << "ms" << endl;
+        // bounding box处理	
         for (int i = 0; i < boxes.size(); i++) {
-            std::cout<<boxes[i].x1<<" "<<boxes[i].y1<<" "<<boxes[i].x2<<" "<<boxes[i].y2
+            std::cout<<i<<" "<<boxes[i].x1<<" "<<boxes[i].y1<<" "<<boxes[i].x2<<" "<<boxes[i].y2
                  <<" "<<boxes[i].score<<" "<<boxes[i].cate<<std::endl;
-        
             char text[256];
             sprintf(text, "%s %.1f%%", class_names[boxes[i].cate], boxes[i].score * 100);
 
@@ -65,8 +79,18 @@ int main()
             cv::rectangle (cvImg, cv::Point(boxes[i].x1, boxes[i].y1), 
                        cv::Point(boxes[i].x2, boxes[i].y2), cv::Scalar(255, 255, 0), 2, 2, 0);
         }
+        if(boxes.size())
+        {
+           cv::Point P;
+           obj_position.centerX = boxes[0].x1+(boxes[0].x2 - boxes[0].x1)*0.5;
+           obj_position.centerY = boxes[0].y1+(boxes[0].y2 - boxes[0].y1)*0.5;
+           box_pub.publish(obj_position);
+           P.x = obj_position.centerX;
+           P.y = obj_position.centerY;
+           cv::circle(cvImg, P, 3,Scalar(255,0,0),-1);
+        }
+        imshow("Bound_box", cvImg);
         
-        imshow("t", cvImg);
         
     }
     
